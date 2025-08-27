54a6f2cf1492214af68c5d81f583231458d14c19
Jackisa Daniel Barack <87069100+bjackisa@users.noreply.github.com>
2025-08-27 20:35:48 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'CWEN Tool',
+  description: 'Collecting every item we need on site and analyzing it',
+  generator: 'jackisa.com',
 }
 
 export default function RootLayout({
